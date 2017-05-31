a819dd77a77e11032dbfdc821f24cd506bbd72c4
sup3rcow <bullandrooster@gmail.com>
2017-05-31 09:46:19 +0200

    updateing Customer model

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -4,5 +4,12 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public int MyProperty { get; set; }
+
+
+        //ovako napravis foreign key u tablici
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }//ef konvencijom, skuzi da je ovo foreign key od MembershipType-a
     }
 }
